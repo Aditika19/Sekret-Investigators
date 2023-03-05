c4d9cc129f22fbda63c52f8496c79e9d77bb8670
Aditika <aditika.verma@vanarts.com>
2023-03-05 03:35:30 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const modeToggle = document.querySelector('#mode-toggle');
+const body = document.querySelector('body');
+
+modeToggle.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+
+});
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 50;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.onload=function(){
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 50;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", reveal);
